a3105f183135cb2ff1cf10f5725000d9c530be93
Marcelo Barcelos <marcelo.ambarc@gmail.com>
2022-08-18 13:29:08 -0300

    Correção de caracteres especiais

--- index.js
+++ index.js
@@ -7,12 +7,48 @@ try {
     if(err){
       console.log(err);
     }
-
+    var blockReplaced = "";
     const block = fs.readFileSync(`../C-HTML/arquivos/${data[0]}`, 'utf8');
+
+    blockReplaced = block.replace("Á","&Aacute;");
+    blockReplaced = block.replace("á","&aacute;");
+    blockReplaced = block.replace("Â","&Acirc;");
+    blockReplaced = block.replace("â","&acirc;");
+    blockReplaced = block.replace("À","&Agrave;");
+    blockReplaced = block.replace("à","&agrave;");
+    blockReplaced = block.replace("Å","&Aring;");
+    blockReplaced = block.replace("å","&aring;");
+    blockReplaced = block.replace("Ã","&Atilde;");
+    blockReplaced = block.replace("ã","&atilde;");
+    blockReplaced = block.replace("Ä","&Auml;");
+    blockReplaced = block.replace("ä","&auml;");
+    blockReplaced = block.replace("Æ","&AElig;");
+    blockReplaced = block.replace("æ","&aelig;");
+    blockReplaced = block.replace("É","&Eacute;");
+    blockReplaced = block.replace("é","&eacute;");
+    blockReplaced = block.replace("Ê","&Ecirc;");
+    blockReplaced = block.replace("ê","&ecirc;");
+    blockReplaced = block.replace("È","&Egrave;");
+    blockReplaced = block.replace("è","&egrave;");
+    blockReplaced = block.replace("Ë","&Euml;");
+    blockReplaced = block.replace("ë","&euml;");
+    blockReplaced = block.replace("Í","&Iacute;");
+    blockReplaced = block.replace("í","&iacute;");
+    blockReplaced = block.replace("í","&iacute;");
+    blockReplaced = block.replace("Ó","&Oacute;");
+    blockReplaced = block.replace("ó","&oacute;");
+    blockReplaced = block.replace("Ô","&Ocirc;");
+    blockReplaced = block.replace("ô","&ocirc;");
+    blockReplaced = block.replace("Õ","&Otilde;");
+    blockReplaced = block.replace("õ","&otilde;");
+    blockReplaced = block.replace("Ú","&Uacute;");
+    blockReplaced = block.replace("ú","&uacute;");
+    blockReplaced = block.replace("Ç","&Ccedil;");
+    blockReplaced = block.replace("ç","&ccedil;");
 
     var options = { format: 'A4', orientation: "landscape" };
 
-    pdf.create(block, options).toBuffer((err, pdfBuffer) => {
+    pdf.create(blockReplaced, options).toBuffer((err, pdfBuffer) => {
       if(err){
         console.log(err);
       }
